6d612fec8342713c34db16e4414ce4e805afb5c1
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-05-04 05:47:39 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -3,7 +3,7 @@
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 # inline credit @keselekpermen69
-# Pengguna Lynx-Userbot
+# Pengguna King-Ubot
 """ Userbot initialization. """
 
 import os
