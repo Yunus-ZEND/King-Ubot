77e86c8cefaa0b456647d3293717af92b0e33bbc
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-05-05 16:07:33 +0700

    Update tempmail.py

--- userbot/modules/tempmail.py
+++ userbot/modules/tempmail.py
@@ -26,7 +26,7 @@ async def _(event):
             await asyncio.sleep(1)
             await conv.send_message("Generate New")
             response = await response
-            lynxuserbot = ((response).reply_markup.rows[2].buttons[0].url)
+            kingubot = ((response).reply_markup.rows[2].buttons[0].url)
             await event.client.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
             await king.edit("`Mohon Maaf, Silahkan Buka` @TempMailBot `Lalu Tekan Start dan Coba Lagi.`")
